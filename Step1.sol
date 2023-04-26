6a2dfe7920d89f38a4c0cf6d34e86a80e6058817
devELIOper <lopeselio@gmail.com>
2023-04-26 11:31:28 -0700

    Update Step1.sol

--- Step1.sol
+++ Step1.sol
@@ -1,1 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.6;
 
+// Step 1: importing Chainlink Automation contracts and Counters library 
+import "@chainlink/contracts/src/v0.8/KeeperCompatible.sol";
+import "@openzeppelin/contracts@4.6.0/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.6.0/utils/Counters.sol";
+
+contract DynamicNFT is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("Dynamic NFT", "DNFT") {}
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+}
